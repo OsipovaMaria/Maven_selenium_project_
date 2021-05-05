89a311b6a501d76de1984dd0a99923a42d0aff77
OsipovaMaria <mariia.osipova@orioninc.com>
2021-05-05 11:14:46 +0300

    Задание 3. Сделайте сценарий логина Сделайте сценарий для логина в панель администрирования учебного приложения http://localhost/litecart/admin/. Проверки можно пока никакие не делать, только действия -- заполнение полей, нажатия на кнопки и ссылки. Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл. Не забывайте о том, что браузер должен останавливаться, даже если возникли какие-либо ошибки во время выполнения сценария. ----- Уложите созданный файл, содержащий сценарий, в ранее созданный репозиторий. В качестве ответа на задание отправьте ссылку на свой репозиторий и указание, какой именно файл содержит нужный сценарий.

--- src/test/java/Lesson1.java
+++ src/test/java/Lesson1.java
@@ -1,20 +1,41 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 
 public class Lesson1 {
-    private static WebDriver driver;
+    private WebDriver driver;
+    private String userNameElementCss = "input[name=username]";
+    private String passwordElementCss = "input[name=password]";
+    private String loginButtonCss = "button[name=login]";
+    private String logoutLinkCss = "a[title=Logout]";
+    private String loginValue = "admin";
+    private int timeout = 5;
 
     @BeforeClass
     public void startDriver(){
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
     }
     @Test
     public void test1(){
         driver.get("http://www.google.com/");
+    }
+    @Test
+    public void test3(){
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.cssSelector(userNameElementCss)).sendKeys(loginValue);
+        driver.findElement(By.cssSelector(passwordElementCss)).sendKeys(loginValue);
+        driver.findElement(By.cssSelector(loginButtonCss)).click();
+        assert(driver.findElement(By.cssSelector(logoutLinkCss)).getText()).matches("Logout");
     }
     @AfterClass
     public void stopDriver(){
