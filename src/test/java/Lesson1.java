6c815a3bf2c652f5e98939e662285beb1d795736
OsipovaMaria <mariia.osipova@orioninc.com>
2021-05-06 08:57:10 +0300

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки Сделайте сценарий, который выполняет следующие действия в учебном приложении litecart. 1) входит в панель администратора http://localhost/litecart/admin 2) прокликивает последовательно все пункты меню слева, включая вложенные пункты 3) для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1) Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл. Если возникают проблемы с выбором локаторов для поиска элементов -- обращайтесь в чат за помощью. ----- Уложите созданный файл, содержащий сценарий, в ранее созданный репозиторий. В качестве ответа на задание отправьте ссылку на свой репозиторий и указание, какой именно файл содержит нужный сценарий.

--- src/test/java/Lesson1.java
+++ src/test/java/Lesson1.java
@@ -1,89 +1,80 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
-
-
 public class Lesson1 {
     private WebDriver driver;
-    private int timeout = 10;
+    private String userNameElementXpath = "//input[@name='username']";
+    private String passwordElementXpath = "//input[@name='password']";
+    private String loginButtonXpath = "//button[@name='login']";
+    private String logoutLinkXpath = "//a[@title='Logout']";
+    private String loginValue = "admin";
+    private int timeout = 15;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
     }
+
     @Test
-    public void test1(){
+    public void test1() {
         driver.get("http://www.google.com/");
     }
-    @Test
-    public void test3(){
-        loginToAdmin();
-        String logoutLinkXpath = "//a[@title='Logout']";
-        assert(driver.findElement(By.xpath(logoutLinkXpath)).getText()).matches("Logout");
-    }
-    @Test
-    public void test7(){
-        loginToAdmin();
 
-        clickApps("appearance");
-        clickDocs("logotype");
-        clickApps("catalog");
-        clickDocs("attribute_groups","manufacturers","suppliers","delivery_statuses","sold_out_statuses","quantity_units","csv");
-        clickApps("countries", "currencies", "customers");
-        clickDocs("csv", "newsletter");
-        clickApps("geo_zones", "languages");
-        clickDocs("storage_encoding");
-        clickApps("modules");
-        clickDocs("shipping","payment","order","order_total","jobs");
-        clickApps("orders");
-        clickDocs("order_statuses");
-        clickApps("pages");
-        clickDocs("csv");
-        clickApps("reports");
-        clickDocs("most_sold_products", "most_shopping_customers");
-        clickApps("settings");
-        clickDocs("defaults","email","listings","customer_details","legal","images","checkout","advanced","security");
-        clickApps("slides", "tax");
-        clickDocs("tax_classes");
-        clickApps("translations");
-        clickDocs("scan", "csv");
+    @Test
+    public void test3() {
+        loginToAdmin();
+        assert (driver.findElement(By.xpath(logoutLinkXpath)).getText()).matches("Logout");
+    }
+
+    @Test
+    public void test7() {
+        loginToAdmin();
+        clickAppAndDoc("appearance", "logotype");
+        clickAppAndDoc("catalog", "attribute_groups", "manufacturers", "suppliers", "delivery_statuses", "sold_out_statuses", "quantity_units", "csv");
+        clickApps("countries", "currencies");
+        clickAppAndDoc("customers", "csv", "newsletter");
+        clickApps("geo_zones");
+        clickAppAndDoc("languages", "storage_encoding");
+        clickAppAndDoc("modules", "shipping", "payment", "order", "order_total", "jobs");
+        clickAppAndDoc("orders", "order_statuses");
+        clickAppAndDoc("pages", "csv");
+        clickAppAndDoc("reports", "most_sold_products", "most_shopping_customers");
+        clickAppAndDoc("settings", "defaults", "email", "listings", "customer_details", "legal", "images", "checkout", "advanced", "security");
+        clickApps("slides");
+        clickAppAndDoc("tax", "tax_classes");
+        clickAppAndDoc("translations", "scan", "csv");
         clickApps("users", "vqmods");
     }
 
-    public void loginToAdmin(){
-        String userNameElementXpath = "//input[@name='username']";
-        String passwordElementXpath = "//input[@name='password']";
-        String loginButtonXpath = "//button[@name='login']";
-        String loginValue = "admin";
+    public void loginToAdmin() {
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.xpath(userNameElementXpath)).sendKeys(loginValue);
         driver.findElement(By.xpath(passwordElementXpath)).sendKeys(loginValue);
         driver.findElement(By.xpath(loginButtonXpath)).click();
     }
-    public void clickApps(String ... params){
+
+    public void clickApps(String... params) {
         List<String> list = Arrays.stream(params).toList();
-        list.forEach(element -> driver.findElement(By.xpath("//*[@class='app' and @data-code='"+element+"']")).click());
+        list.forEach(element -> driver.findElement(By.xpath("//*[@class='app' and @data-code='" + element + "']")).click());
     }
 
-    public void clickDocs(String ... params){
-        List<String> list = Arrays.stream(params).toList();
-        list.forEach(element -> driver.findElement(By.xpath("//*[@class='doc' and @data-code='"+element+"']")).click());
+    public void clickAppAndDoc(String classElement, String... docParams) {
+        List<String> list = Arrays.stream(docParams).toList();
+        driver.findElement(By.xpath("//*[@class='app' and @data-code='" + classElement + "']")).click();
+        list.forEach(element -> driver.findElement(By.xpath("//*[@class='doc' and @data-code='" + element + "']")).click());
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
