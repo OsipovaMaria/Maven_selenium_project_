f9bf492f59d77ffbcbb1eb3d7411d2c3305bbd2f
OsipovaMaria <mariia.osipova@orioninc.com>
2021-05-07 11:07:47 +0300

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки Сделайте сценарий, который выполняет следующие действия в учебном приложении litecart. 1) входит в панель администратора http://localhost/litecart/admin 2) прокликивает последовательно все пункты меню слева, включая вложенные пункты 3) для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)

--- src/test/java/Lesson1.java
+++ src/test/java/Lesson1.java
@@ -4,20 +4,44 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
+
 
 public class Lesson1 {
-    private static WebDriver driver;
+    private WebDriver driver;
+    private final int timeout = 5;
 
     @BeforeClass
-    public void startDriver(){
+    public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
     }
+
     @Test
-    public void test1(){
-        driver.get("http://www.google.com/");
+    public void test1() {
+        Utils.openPage(driver, "http://www.google.com/");
     }
+
+    @Test
+    public void test3() {
+        Utils.loginAsAdminUser(driver, "http://localhost/litecart/admin/login.php");
+    }
+
+    @Test
+    public void test7() {
+        Utils.loginAsAdminUser(driver, "http://localhost/litecart/admin/login.php");
+        MenuPage.menuItemsList().forEach(item -> {
+            Utils.clickOnMenuItem(driver, item.getDataCode());
+            if (item.isSubMenuPresent()) {
+                List<MenuPage> subMenuItems = MenuPage.getSubMenuItems(item);
+                subMenuItems.forEach(subMenuItem -> Utils.clickOnMenuItem(driver, subMenuItem.getDataCode()));
+            }
+        });
+    }
+
     @AfterClass
-    public void stopDriver(){
+    public void tearDown() {
         driver.quit();
     }
 }
