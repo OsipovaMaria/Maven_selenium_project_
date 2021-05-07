019015081d7b7e8637199dd942a1f60c5d2d62d9
OsipovaMaria <mariia.osipova@orioninc.com>
2021-05-07 11:36:20 +0300

    5. Научитесь запускать Firefox "по старой схеме" Попробуйте запустить разработанный ранее сценарий логина в браузере Firefox ESR, используя старую схему запуска, без использования geckodriver. Если Selenium не может сам найти место, куда установлен Firefox ESR -- укажите в параметрах запуска путь к браузеру.
    Задание 6. Научитесь запускать Firefox Nightly
    Попробуйте запустить разработанный ранее сценарий логина в браузере Firefox Nightly (https://nightly.mozilla.org/). Если Selenium не может сам найти место, куда установлен Firefox Nightly -- укажите в параметрах запуска путь к браузеру.

--- src/test/java/Lesson1.java
+++ src/test/java/Lesson1.java
@@ -1,32 +1,52 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Lesson1 {
     private WebDriver driver;
     private final int timeout = 5;
+
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
     }
+
     @Test
     public void test1() {
         Utils.openPage(driver, "http://www.google.com/");
     }
+
     @Test
     public void test3() {
         Utils.loginAsAdminUser(driver, "http://localhost/litecart/admin/login.php");
     }
+
+    @Test
+    public void test5() {
+        WebDriver driverESR;
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driverESR = new FirefoxDriver(caps);
+        Utils.loginAsAdminUser(driverESR, "http://localhost/litecart/admin/login.php");
+    }
+
+    @Test
+    public void test6() {
+        WebDriver driverESR;
+        System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver.exe");
+        driverESR = new FirefoxDriver(new FirefoxOptions().setBinary("C:\\Program Files\\Firefox Nightly\\firefox.exe"));
+        Utils.loginAsAdminUser(driverESR, "http://localhost/litecart/admin/login.php");
+    }
+
     @Test
     public void test7() {
         Utils.loginAsAdminUser(driver, "http://localhost/litecart/admin/login.php");
@@ -37,9 +57,10 @@ public class Lesson1 {
                 subMenuItems.forEach(subMenuItem -> Utils.clickOnMenuItem(driver, subMenuItem.getDataCode()));
             }
         });
-    }   
+    }
+
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
